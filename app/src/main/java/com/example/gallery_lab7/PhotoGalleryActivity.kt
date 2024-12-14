4f210b923918c1dfef4cb2bb462d4b6562c71274
Lev <kartoxauaua@gmail.com>
2024-12-14 20:59:45 +0700

    Общие настройки

--- app/src/main/java/com/example/gallery_lab7/PhotoGalleryActivity.kt
+++ app/src/main/java/com/example/gallery_lab7/PhotoGalleryActivity.kt
@@ -1,10 +1,7 @@
 package com.example.gallery_lab7
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PhotoGalleryActivity :
     AppCompatActivity() {
--- app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
@@ -7,15 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.gallery_lab7.api.FlickrApi
+import retrofit2.Retrofit
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoRecyclerView:
             RecyclerView
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
         val view =
             inflater.inflate(R.layout.fragment_photo_gallery, container, false)
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
@@ -25,5 +23,15 @@ class PhotoGalleryFragment : Fragment() {
     companion object {
         fun newInstance() =
             PhotoGalleryFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val retrofit: Retrofit =
+            Retrofit.Builder()
+                .baseUrl("https://www.flickr.com/")
+                .build()
+        val flickrApi: FlickrApi =
+            retrofit.create(FlickrApi::class.java)
     }
 }
