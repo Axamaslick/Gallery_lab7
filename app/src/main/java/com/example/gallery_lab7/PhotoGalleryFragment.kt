6e58ca5a7aec51975b8910c64467bdc0b94dc0d0
Lev <kartoxauaua@gmail.com>
2024-12-14 20:21:03 +0700

    Настройка PhotoGalleryFragment

--- app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
@@ -1,4 +1,25 @@
 package com.example.gallery_lab7
 
-class PhotoGalleryFragment {
+
+class PhotoGalleryFragment : Fragment() {
+    private lateinit var photoRecyclerView:
+            RecyclerView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view =
+            inflater.inflate(R.layout.fragment_photo_galler
+                    y, container, false)
+        photoRecyclerView =
+            view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager =
+            GridLayoutManager(context, 3)
+        return view
+    }
+    companion object {
+        fun newInstance() =
+            PhotoGalleryFragment()
+    }
 }
