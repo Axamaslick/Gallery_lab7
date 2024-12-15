7680d00dacf02be7224d70c69f8357777f61f67e
Lev <kartoxauaua@gmail.com>
2024-12-15 22:46:35 +0700

    Общие изменения

--- app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/gallery_lab7/PhotoGalleryFragment.kt
@@ -1,9 +1,11 @@
 package com.example.gallery_lab7
 
 import FlickrFetchr
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -72,7 +74,11 @@ class PhotoGalleryFragment : Fragment() {
         retainInstance = true
         photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
 
-        thumbnailDownloader = ThumbnailDownloader()
+        val responseHandler = Handler()
+        thumbnailDownloader = ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+                val drawable = BitmapDrawable(resources, bitmap)
+                photoHolder.bindDrawable(drawable)
+            }
         lifecycle.addObserver(thumbnailDownloader)
 
     }
