f899b63cfa9480a044606f7afebd41d27e82c387
Lev <kartoxauaua@gmail.com>
2024-12-15 21:02:33 +0700

    Настройка FlickerApi

--- app/src/main/java/com/example/gallery_lab7/FlickrFetchr.kt
+++ app/src/main/java/com/example/gallery_lab7/FlickrFetchr.kt
@@ -1,3 +1,25 @@
+
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gallery_lab7.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+
+
 private const val TAG = "FlickrFetchr"
 class FlickrFetchr {
     private val flickrApi: FlickrApi
@@ -8,7 +30,8 @@ class FlickrFetchr {
 
     fun fetchContents(): LiveData<String> {
         val responseLiveData: MutableLiveData<String> = MutableLiveData()
-        val flickrRequest: Call<String> = flickrApi.fetchContents()flickrRequest.enqueue(object : Callback<String> {
+        val flickrRequest: Call<String> = flickrApi.fetchContents()
+        flickrRequest.enqueue(object : Callback<String> {
             override fun onFailure(call: Call<String>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch photos", t)
             }
--- app/src/main/java/com/example/gallery_lab7/api/FlickrApi.kt
+++ app/src/main/java/com/example/gallery_lab7/api/FlickrApi.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET("services/rest/?method=flickr.interestingness.getList" + "&api_key=858ef0a6ed868feebcbadf152781b635" + "&format=json" + "&nojsoncallback=1" + "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
 }
